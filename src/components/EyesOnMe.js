8cc467e9779b881e046db2480ffb51c91828bc1d
Shania Lopez <slopez@fandm.edu>
2023-06-19 15:55:28 -0400

    complete lab

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,17 @@
 // Code EyesOnMe Component Here
+import React from "react"
+function handleFocus(){
+    return console.log('Good!')
+}
+
+function handleBlur() {
+return console.log('Hey! Eyes on me!')
+}
+function EyesOnMe() {
+    return <>
+    <button onFocus={handleFocus} onBlur={handleBlur}>Eyes on me</button>
+    </>
+    
+}
+
+export default EyesOnMe;
